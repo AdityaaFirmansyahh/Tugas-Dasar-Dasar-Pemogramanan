c0bc8d07e2d022abddd676d93c178f5259545b34
AdityaaFirmansyahh <145969890+AdityaaFirmansyahh@users.noreply.github.com>
2023-10-31 15:26:57 +0700

    Add files via upload

---
+++ Tugas_P5_03.py
@@ -0,0 +1,10 @@
+print('''
+Silahkan pilih tools dibawah ini dengan mengirimkan nomernya
+===============
+tools yang tersedia
+===============
+1.luas persegi
+2.luas lingkaran
+3.luas segitiga
+''')
+a = int(input("masukan pilihan mu : "))
---
+++ Tugas_P5_1&2.py
@@ -0,0 +1,13 @@
+kendaraan =["b 4030 rfs","beat","150","black"]
+kendaraan.append("30 juta")
+kendaraan.append("2 roda")
+kendaraan.insert(2,"honda")
+kendaraan.insert(3,"motor matic")
+print(kendaraan [0])
+print(kendaraan [1])
+print(kendaraan [2])
+print(kendaraan [3])
+print(kendaraan [4])
+print(kendaraan [5])
+print(kendaraan [6])
+print(kendaraan [7])
