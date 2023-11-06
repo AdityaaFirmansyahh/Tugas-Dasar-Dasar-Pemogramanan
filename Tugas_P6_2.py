cb958de6eb2984a0bc124da1fe939889c14f6298
AdityaaFirmansyahh <145969890+AdityaaFirmansyahh@users.noreply.github.com>
2023-11-06 22:02:57 +0700

    Add files via upload

---
+++ Tugas_P6_2.py
@@ -0,0 +1,6 @@
+hasil = 0
+for i in range(1, 20, 2):
+    hasil += i
+    print(i,end=(' + '))
+print('=', hasil)
+print('1 + 3 + 5 + 7 + 9 + 11 + 13 + 15 + 17 + 19=', hasil)
---
+++ Tugas_P6_3.py
@@ -0,0 +1,5 @@
+value= int(input('masukan jumlah baris: '))
+for a in range (value):
+    for b in range(a+1):
+        print(' *', end='')
+    print()
